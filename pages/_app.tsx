5fc7fd493b8b8cc5f1ceb384c0c448648316b93a
Ginkoe <ginkoe@protonmail.com>
2022-01-08 22:51:02 +0100

    setup linter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import React from "react";
+
+import type { NextComponentType } from "next";
+import type { AppProps } from "next/app";
+
+interface CustomAppProps extends AppProps {
+  Component: NextComponentType;
+  pageProps: Record<string, unknown>;
 }
 
-export default MyApp
+const MyApp: React.FC<CustomAppProps> = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
