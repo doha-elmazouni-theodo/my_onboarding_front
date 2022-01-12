f4ea37945a49ebdab7d05f6811579b2404dc59b9
Mohamed Adam Belcaid <mohamed-adam.belcaid@nimbleways.com>
2022-01-12 15:20:46 +0100

    sample landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import Banner3S from "~components/landing/banner3s";
 import { SITE_CONFIG } from "~config/site";
 import type { NextPage } from "~types/next";
 
@@ -10,8 +11,47 @@ const Home: NextPage = () => {
     <div>
       <NextSeo title="Home" />
 
-      <h1>{SITE_CONFIG.title}</h1>
-      <h2>{SITE_CONFIG.description}</h2>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="flex flex-col text-center w-full mb-20">
+            <h2 className="text-xs text-red-500 tracking-widest font-medium title-font mb-1">
+              {SITE_CONFIG.title.toUpperCase()}
+            </h2>
+            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+              {SITE_CONFIG.description}
+            </h1>
+          </div>
+          <div className="flex flex-wrap justify-center">
+            <Banner3S
+              title="Stability"
+              doc={
+                "https://www.notion.so/m33/Framework-Qualit-3S-3bff76b2f374475298ef909341696281#77f96802a1954504a71ca6480f609c50"
+              }
+            >
+              Le produit ne doit pas présenter de bugs impactant l'expérience
+              utilisateur.
+            </Banner3S>
+            <Banner3S
+              title="Security"
+              doc={
+                "https://www.notion.so/m33/Framework-Qualit-3S-3bff76b2f374475298ef909341696281#becfbc6cab35431fbef11110c9b34546"
+              }
+            >
+              Le produit doit être prémunis contre les vulnérabilités les plus
+              communes.
+            </Banner3S>
+            <Banner3S
+              title="Speed"
+              doc={
+                "https://www.notion.so/m33/Framework-Qualit-3S-3bff76b2f374475298ef909341696281#b9184c326e2c48d6a696ecd59f64328f"
+              }
+            >
+              Le produit doit être au standard sur la performance. Ca inclut la
+              vitesse de chargement et la scalabilité.
+            </Banner3S>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
