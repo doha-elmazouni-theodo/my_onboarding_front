412e1ab4a766f2baa4945a35b2014db3c69b6ed0
Othmane Kinane <76955412+othmane-kinane-nw@users.noreply.github.com>
2023-08-17 14:33:26 +0100

    lint-staged: fix config to match Nextjs doc

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,13 @@
 // @ts-nocheck
+const path = require('path')
+
+// from: https://nextjs.org/docs/pages/building-your-application/configuring/eslint#lint-staged
+const buildEslintCommand = (filenames) =>
+  `pnpm lint --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`
 
 module.exports = {
-  "**/*": (filename) => "yarn lint --file " + filename,
-};
+  // default ESLint extensions, to be in sync with the pipeline command
+  '*.{js,mjs,cjs,jsx,ts,mts,cts,tsx}': [buildEslintCommand],
+}
