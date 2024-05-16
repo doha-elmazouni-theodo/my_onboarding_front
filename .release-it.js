4ce96ce6d484a5078c839cc970215a3ac0523252
ilyass-nasr-nw <112965128+ilyass-nasr-nw@users.noreply.github.com>
2024-05-16 22:57:33 +0100

    chore(config): :wrench: add release it helper (#165)

---
+++ .release-it.js
@@ -0,0 +1,41 @@
+console.warn(
+  "\x1b[33m%s\x1b[0m",
+  `If this is your first release, you might want to take a look at .release-it.js
+This command will fail if you are not on main, if you want to change the branch,
+change the requireBranch value in .release-it.js or turn off the requireBranch option.
+Happy releasing 🚀!`,
+);
+
+// https://github.com/release-it/release-it/blob/main/docs/configuration.md
+module.exports = {
+  plugins: {
+    // https://github.com/release-it/conventional-changelog?tab=readme-ov-file
+    "@release-it/conventional-changelog": {
+      ignoreRecommendedBump: true,
+      infile: "CHANGELOG.md",
+      preset: {
+        name: "conventionalcommits",
+      },
+    },
+  },
+  git: {
+    commitMessage: "chore: :bookmark: release v${version}",
+    tagName: "v${version}",
+    requireCommits: true,
+    push: false,
+    /**
+     * It is recommended to require a branch to be checked out before releasing.
+     * This is to prevent releasing from a detached HEAD.
+     */
+    requireBranch: "main",
+  },
+  /**
+   * Additonally, you can also automate the creation of GitHub or GitLab releases.
+   * github: {
+   *  release: true,
+   * },
+   * gitlab: {
+   *  release: true,
+   * },
+   */
+};
